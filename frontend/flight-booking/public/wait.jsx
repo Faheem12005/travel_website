5491647182355a89c44f1ddfc13009def5c296c1
Mohammed <mohdfaheem.1205@gmail.com>
2024-06-21 00:39:05 +0530

    made ui more cleaner

---
+++ frontend/flight-booking/public/wait.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+const WaitIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    x="0px"
+    y="0px"
+    viewBox="0 0 512 640"
+    style={{ enableBackground: 'new 0 0 512 512' }}
+    xmlSpace="preserve"
+    id="svg2"
+    className={''}
+    {...props}
+  >
+    <metadata id="metadata24">
+      <rdf:RDF>
+        <cc:Work rdf:about="">
+          <dc:format>image/svg+xml</dc:format>
+          <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+          <dc:title></dc:title>
+        </cc:Work>
+      </rdf:RDF>
+    </metadata>
+    <defs id="defs22" />
+    <sodipodi:namedview
+      pagecolor="#ffffff"
+      bordercolor="#666666"
+      borderopacity="1"
+      objecttolerance="10"
+      gridtolerance="10"
+      guidetolerance="10"
+      inkscape:pageopacity="0"
+      inkscape:pageshadow="2"
+      inkscape:window-width="1680"
+      inkscape:window-height="988"
+      id="namedview20"
+      showgrid="false"
+      inkscape:zoom="0.7375"
+      inkscape:cx="148.31457"
+      inkscape:cy="415.35045"
+      inkscape:window-x="-8"
+      inkscape:window-y="-8"
+      inkscape:window-maximized="1"
+      inkscape:current-layer="svg2"
+    />
+    <path
+    className='fill-current'
+      d="m 292.2,435.8 c -3.6,0 -6.1,0 -8.6,0 -27.7,-0.1 -55.3,0.3 -83,-0.3 -45.6,-0.9 -85.1,-32.6 -96.3,-76.7 -9.2,-36.1 -18.1,-72.4 -27.1,-108.6 -6.4,-25.7 6.8,-48.9 31.2,-55.3 24,-6.3 47.4,8.2 53.9,33.6 9,35.2 17.8,70.5 26.7,105.8 2.4,9.7 7.2,13.4 17.3,13.4 35.3,0 70.7,0 106,0 26.7,0 45.6,18.9 45.7,45.6 0,48.6 0,97.2 0,145.8 0,19.6 -13.9,34.3 -32.5,34.5 -18.7,0.2 -33,-14.1 -33.2,-33.8 -0.2,-31.9 -0.1,-63.7 -0.1,-95.6 0,-2.3 0,-4.8 0,-8.4 z"
+      id="path4"
+      inkscape:connector-curvature="0"
+    />
+    <path
+    className='fill-current'
+      d="m 262.4,186.9 c -0.2,-63.4 51.7,-115.4 115,-115.5 63.1,-0.1 115.6,51.4 115.7,113.4 0.1,64.5 -50.6,116 -113.3,117.2 -60,1.2 -116.9,-47 -117.4,-115.1 z m 115.9,-82.3 c -45.9,-0.1 -82.9,36.3 -83,81.6 -0.1,45.4 36.6,82.7 81.9,83.2 45.4,0.5 82.9,-37 82.8,-82.7 -0.1,-45.5 -36.5,-82 -81.7,-82.1 z"
+      id="path6"
+      inkscape:connector-curvature="0"
+    />
+    <path
+    className='fill-current'
+      d="m 208.4,490.6 c -24.4,1.7 -52,-1.7 -77.7,-15 -44,-22.9 -74,-57.1 -85.8,-106.3 -7.6,-31.7 -15.9,-63.3 -23.8,-95 -2.5,-10 2.4,-19 11.4,-21.3 9.2,-2.3 17.6,2.9 20.3,13.2 8.3,32.3 16.5,64.6 24.5,97 13.4,54.7 63.9,94.6 120.3,94.7 15.8,0 31.6,0 47.4,0 10.6,0 17.8,6.6 18,16.2 0.1,9.1 -6.8,16.1 -17,16.4 -11.2,0.4 -22.3,0.1 -37.6,0.1 z"
+      id="path8"
+      inkscape:connector-curvature="0"
+    />
+    <path
+    className='fill-current'
+      d="m 109,177.6 c -27.1,0 -49.3,-22 -49.5,-49 -0.1,-27 22.9,-49.8 49.8,-49.4 27.2,0.4 49.1,22.5 49,49.5 -0.1,27 -22.1,48.9 -49.3,48.9 z"
+      id="path10"
+      inkscape:connector-curvature="0"
+    />
+    <path
+    className='fill-current'
+      d="m 371.9,185.4 c 14.8,-15 28.7,-29 42.6,-42.9 5.2,-5.2 10.7,-5.7 15.4,-1.7 5,4.1 5.8,11 0.9,16 -16.9,17.2 -34,34.3 -51.2,51.2 -4.7,4.6 -10.4,4.2 -15,-0.2 -11.5,-11.1 -22.9,-22.4 -34,-34 -4.8,-5 -4.3,-11.4 0.2,-15.6 4.5,-4.2 10.6,-4.1 15.7,0.6 4.4,4.1 8.7,8.4 12.8,12.8 4.1,4.3 8,8.8 12.6,13.8 z"
+      id="path12"
+      inkscape:connector-curvature="0"
+    />
+    <path
+    className='fill-current'
+      d="m 241.7,575 -95.1,0 c -8.3,0 -15,-6.8 -15,-15 l 0,-30 c 0,-8.3 6.8,-15 15,-15 l 95.1,0 c 8.3,0 15,6.8 15,15 l 0,30 c 0,8.3 -6.7,15 -15,15 z"
+      id="path14"
+      inkscape:connector-curvature="0"
+    />
+  </svg>
+);
+
+export default WaitIcon;
--- frontend/flight-booking/src/Offer.jsx
+++ frontend/flight-booking/src/Offer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { format, differenceInMinutes } from 'date-fns';
+import WaitIcon from "../public/wait";
 
 function Offer({data}) {
     const formatDuration = (duration) => {
@@ -8,38 +10,114 @@ function Offer({data}) {
         return `${hours}h ${minutes}m`;
     };
 
+
+    const formatDeparture = (time) => {
+        const string = time.split('T')[1].slice(0,5);
+        return string;
+    }
+
+    const formatArrival = (arrival,departure) => {
+        const ar = parseInt(arrival.split('T')[0].slice(8,10));
+        const dp = parseInt(departure.split('T')[0].slice(8,10));
+        if (!ar>dp){ return(arrival.split('T')[1].slice(0,5)) }
+        else if (ar-dp == 1){
+            return(
+                <div>{arrival.split('T')[1].slice(0,5)}<span className="text-gray-400 font text-sm font-extralight">+1D</span></div>)
+        }
+        else if (ar-dp == 2){
+            return(
+                <div>{arrival.split('T')[1].slice(0,5)}<span className="text-gray-400 font text-sm font-extralight">+2D</span></div>)
+        }
+        else if (ar-dp == 3){
+            return(
+                <div>{arrival.split('T')[1].slice(0,5)}<span className="text-gray-400 font text-sm font-extralight">+3D</span></div>)
+        }
+    }
+
+    const calculateLayover = (layover,arrival, departure) => {
+        const arrivalTime = new Date(arrival);
+        const departureTime = new Date(departure);
+        const diffMinutes = differenceInMinutes(departureTime, arrivalTime);
+        const hours = Math.floor(diffMinutes / 60);
+        const minutes = diffMinutes % 60;
+        if (hours>4) {
+            return(
+                <div className="flex flex-col items-center justify-center group">
+                    <WaitIcon width={25} height={25}></WaitIcon>
+                    <div className="opacity-0 flex flex-col items-center group-hover:opacity-100 transition-opacity duration-300">
+                        <div className="text-xs">{hours}h {minutes}m</div>
+                        <div className="text-xs">{layover}</div>
+                    </div>
+
+                </div>
+                
+                
+            );
+        } else {
+            return(
+                <div className="flex flex-col items-center justify-center group">
+                    <svg id="Icons_Run" className={""} overflow="hidden" version="1.1" width="25" height='25' viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><g><circle className="fill-current" cx="64.2" cy="13" r="8"/><path className="fill-current" d=" M 83.6 24 C 81.6 23 79.2 23.7 78.2 25.7 L 74.7 32.3 L 59.3 22.6 C 58.7 22.2 58 22 57.2 22 L 40.2 22 C 38.7 22 37.4 22.8 36.7 24.1 L 30.2 36.1 C 29.1 38 29.9 40.5 31.8 41.5 C 32.4 41.8 33.1 42 33.7 42 C 35.1 42 36.5 41.2 37.2 39.9 L 42.6 30 L 48.5 30 L 30.8 63 L 14.2 63 C 12 63 10.2 64.8 10.2 67 C 10.2 69.2 12 71 14.2 71 L 33.2 71 C 34.7 71 36 70.2 36.7 68.9 L 43.7 56 L 55.2 66.7 L 54.3 86.8 C 54.1 89 55.8 90.9 58 91 C 58.1 91 58.1 91 58.2 91 C 60.3 91 62.1 89.3 62.2 87.2 L 63.2 65.2 C 63.3 64 62.8 62.9 61.9 62.1 L 52.2 53.1 L 62.4 34.1 L 74 41.4 C 74.9 42 76.1 42.2 77.2 41.9 C 78.3 41.6 79.2 40.9 79.7 39.9 L 85.2 29.4 C 86.3 27.5 85.6 25.1 83.6 24 Z"/></g></svg>
+                    <div className="opacity-0 flex flex-col items-center group-hover:opacity-100 transition-opacity duration-300">
+                        <div className="text-xs">{hours}h {minutes}m</div>
+                        <div className="text-xs">{layover}</div>
+                    </div>
+                </div>
+            )
+        }
+    };
+
+    console.log(data.data[7])
+
     return (
         <div className="container mx-auto p-6">
             <h1 className="text-3xl font-bold mb-6 text-center">Flight Offers</h1>
             {
                 data.data.map((offer, index) => (
-                    <div key={index} className="bg-white shadow-sm rounded-lg p-6 mb-6 max-w-5xl min-w-fit border">
+                    <div key={index} className="bg-white shadow-sm rounded-lg px-6 py-3 mb-6 max-w-5xl h-32 min-w-fit border">
                         <div className="grid sm:grid-cols-3 grid-cols-1">
                             {/* Grid column 1: Flight details */}
-                            <div className="col-span-1">
+                            <div className="col-span-1 flex justify-self-start items-center">
                                 <div className="flex items-center space-x-2">
-                                    {offer.itineraries[0].segments.map((flights, flightIndex) => (
-                                        <React.Fragment key={`${index}-${flightIndex}`}>
-                                            {/* Show departure code of the first segment and arrival codes of subsequent segments */}
-                                            {flightIndex === 0 && (
-                                                <div className="text-lg font-semibold">{flights.departure.iataCode}</div>
-                                            )}
-                                            <svg className="w-4 h-4 text-gray-500 mx-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <div className="text-lg font-semibold">{offer.itineraries[0].segments[0].departure.iataCode}</div>
+                                    <svg className="w-4 h-4 text-gray-500 mx-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
-                                            </svg>
-                                            <div className="text-lg font-semibold">{flights.arrival.iataCode}</div>
-                                        </React.Fragment>
-                                    ))}
+                                    </svg>
+                                    <div className="text-lg font-semibold">{offer.itineraries[0].segments[offer.itineraries[0].segments.length-1].arrival.iataCode}</div>
                                 </div>
                             </div>
                             {/* Grid column 2: Duration */}
-                            <div className="col-span-1 flex items-center justify-center">
-                                <div className="text-lg font-semibold">{formatDuration(offer.itineraries[0].duration)}</div>
+                            <div className="col-span-1">
+                                <div className="flex items-center justify-center">
+                                    <div className="mr-4 text-xl font-semibold">{formatDeparture(offer.itineraries[0].segments[0].departure.at)}</div>
+                                    <div className="flex justify-center items-center flex-col text-gray-500">
+                                        <p className="text-sm">Duration</p>
+                                        <svg
+                                            className="h-px w-16 mx-auto mt-2" // Adjust height, width, and margins as needed
+                                            viewBox="0 0 100 1"
+                                            fill="currentColor"
+                                            aria-hidden="true"
+                                        >
+                                            <rect width="100" height="1" rx="0.5" />
+                                        </svg>
+                                        <div className="text-lg">{formatDuration(offer.itineraries[0].duration)}</div>
+                                        <div className="flex flex-row">
+                                            {offer.itineraries[0].segments.map((flights, flightIndex) => (
+                                                <React.Fragment key={`${index}-${flightIndex}`}>
+                                                    {flightIndex >0 && (
+                                                        <div>{calculateLayover(flights.departure.iataCode,offer.itineraries[0].segments[flightIndex-1].arrival.at,offer.itineraries[0].segments[flightIndex].departure.at)}</div>
+                                                    )}
+                                                </React.Fragment>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    <div className="ml-4 text-xl font-semibold">{formatArrival(offer.itineraries[0].segments[offer.itineraries[0].segments.length - 1].arrival.at,offer.itineraries[0].segments[0].departure.at)}</div>
+                                </div>
                             </div>
                             {/* Grid column 3: Price */}
-                            <div className="col-span-1 flex items-center justify-end mt-2">
-                                <span className="text-indigo-600">€{offer.price.total}</span>
+                            <div className="col-span-1 flex items-center justify-end mt-2 space-x-5">
+                                <span className="text-xl font-bold">€{offer.price.total}</span>
                                 {/* {offer.price.currency} */}
+                                <button className="px-14 py-4 bg-amber-500 rounded-xl">Book</button>
                             </div>
                         </div>
                     </div>
